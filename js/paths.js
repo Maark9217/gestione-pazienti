3eda6340b213643442bf3bbc5d7568d1f38613da
Maark9217 <marcocassara92@icloud.com>
2025-04-16 18:54:37 +0200

    Aggiornati i percorsi per GitHub Pages

--- js/paths.js
+++ js/paths.js
@@ -2,5 +2,6 @@ export const isGitHub = window.location.hostname.includes('github.io');
 export const baseUrl = isGitHub ? '/gestione-pazienti' : '';
 
 export function getPath(path) {
-    return `${baseUrl}/${path}`.replace(/\/+/g, '/');
+    const basePath = baseUrl || '';
+    return `${basePath}/${path}`.replace(/\/+/g, '/');
 }
